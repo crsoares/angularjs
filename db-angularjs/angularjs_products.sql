1994f4db20156d62960550178b55f51189c55ec5
crysthianophp <crysthianophp@gmail.com>
2014-09-23 18:07:22 -0300

    db estudos

--- db-angularjs/angularjs_products.sql
+++ db-angularjs/angularjs_products.sql
@@ -31,7 +31,7 @@ CREATE TABLE `products` (
   `category` varchar(45) NOT NULL,
   `price` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Kayak','Um barco para uma pessoa','Esportes Aquaticos',275),(2,'Colete','salva-vidas de protecao e moda','Esportes Aquaticos',48.95),(3,'Bola de Futebol','O tamanho e peso da FIFA aprovado','Futebol',19.5),(4,'Canto Bandeiras','De o seu campo de jogo um toque profissional','Futebol',34.95),(5,'Estadio','Estadio de 35.000 lugares Flat-embalados','Futebol',79500),(6,'pensando Cap','Melhorar a eficiencia do cerebro em 75%','Xadrez',16),(7,'Presidente unsteady','Secretamente dar o seu oponente em desvantage','Xadrez',29.95),(8,'Tabuleiro de Xadrez Humano','Um jogo divertido para a familia','Xadrez',75),(9,'Bling Bling-Rei','Rei cravejado de diamantes banhado a ouro','Xadrez',1200);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-09-23 15:55:21
+-- Dump completed on 2014-09-23 18:07:01
