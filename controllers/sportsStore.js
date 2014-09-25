c7e20650284389acde0eff9a04375f1043b462ab
crysthianophp <crysthianophp@gmail.com>
2014-09-25 18:00:46 -0300

    novos exemplos angularjs

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,6 +1,7 @@
 angular.module("sportsStore")
 .constant("dataUrl", "http://localhost/angularjs/src/model/products.php")
-.controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+.constant("orderUrl", "http://localhost/angularjs/src/model/orders.php")
+.controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
 
 	$scope.data = {};
 
@@ -11,5 +12,22 @@ angular.module("sportsStore")
 		 .error(function (error) {
 		 	$scope.data.error = error;
 		 });
+
+	$scope.sendOrder = function (shippingDetails) {
+		var order = angular.copy(shippingDetails);
+		order.products = cart.getProducts();
+		$http.post(orderUrl, order)
+			 .success(function (data) {
+			 	//alert(data);
+			 	$scope.data.orderId = data;
+			 	cart.getProducts().length = 0;
+			 })
+			 .error(function (error) {
+			 	$scope.data.orderError = error;
+			 })
+			 .finally(function () {
+			 	$location.path("/complete")
+			 });
+	}
 
 });
