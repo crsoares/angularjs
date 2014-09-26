0fbee9f298913b03a15b0b779d8c137b9c1b3c36
crysthianophp <crysthianophp@gmail.com>
2014-09-26 00:08:05 -0300

    estudos em casa dia 25

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module("sportsStoreAdmin")
 .constant("authUrl", "http://localhost/angularjs/src/model/login.php")
+.constant("ordersUrl", "http://localhost/angularjs/src/model/orders.php")
 .controller("authCtrl", function ($scope, $http, $location, authUrl) {
 
 	$scope.authenticate = function (user, pass) {
@@ -24,5 +25,32 @@ angular.module("sportsStoreAdmin")
 	$scope.getScreen = function () {
 		return $scope.current == "Products" ? "/angularjs/views/adminProducts.html" : "/angularjs/views/adminOrders.html";
  	}
+
+})
+.controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+
+	$http.get(ordersUrl, {withCredentials: true})
+		 .success(function (data) {
+		 	$scope.orders = data;
+		 })
+		 .error(function (error) {
+		 	$scope.error = error;
+		 });
+
+	$scope.selectedOrder;
+
+	$scope.selectOrder = function (order) {
+		$scope.selectedOrder = order;
+		/*$scope.selectedOrder.products = JSON.parse($scope.selectedOrder.products);*/
+		alert($scope.selectedOrder.products);
+	};
+
+	$scope.calcTotal = function (order) {
+		var total = 0;
+		for (var i = 0; i < order.products.length; i++) {
+			total += order.products[i].count * order.products[i].price;
+		}
+		return total;
+	}
 
 });
--- src/model/orders.php
+++ src/model/orders.php
@@ -5,21 +5,36 @@ $data = json_decode($data);
 /*echo $data->street;die;
 print_r($data);die;*/
 
-try {
-	$db = new PDO('mysql:dbname=angularjs;host=localhost', 'root', '');
-	$sql = "INSERT INTO orders (name, street, city, zip, country, giftwrap, products) VALUES (?, ?, ?, ?, ?, ?, ?)";
-	$result = $db->prepare($sql);
-	$result->bindParam(1, $data->name);
-	   $result->bindParam(2, $data->street);
-	   $result->bindParam(3, $data->city);
-	   $result->bindParam(4, $data->zip);
-	   $result->bindParam(5, $data->country);
-	   $result->bindParam(6, $data->giftwrap);
-	   $result->bindParam(7, $data->products);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	try {
+		$db = new PDO('mysql:dbname=angularjs;host=localhost', 'root', '');
+		$sql = "INSERT INTO orders (name, street, city, zip, country, giftwrap, products) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		$result = $db->prepare($sql);
+		$result->bindParam(1, $data->name);
+		   $result->bindParam(2, $data->street);
+		   $result->bindParam(3, $data->city);
+		   $result->bindParam(4, $data->zip);
+		   $result->bindParam(5, $data->country);
+		   $result->bindParam(6, $data->giftwrap);
+		   $result->bindParam(7, serialize($data->products));
 
-	$result->execute();
-	echo $db->lastInsertId();
-	//echo file_get_contents("php://input");
-}catch(PDOException $e) {
-	echo 'Error: ' . $e->getMessage();
+		$result->execute();
+		echo $db->lastInsertId();
+		//echo file_get_contents("php://input");
+	}catch(PDOException $e) {
+		echo 'Error: ' . $e->getMessage();
+	}
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	try {
+		$db = new PDO('mysql:dbname=angularjs;host=localhost', 'root', '');
+		$query = $db->prepare("SELECT * FROM orders");
+		$query->execute();
+		$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+		echo json_encode($result);
+
+	}catch(PDOException $e) {
+		echo 'Error: ' . $e->getMessage();
+	}
 }
+
